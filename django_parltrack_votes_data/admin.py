998513cee85499a379f8c6b12674e5cdc51ddcfb
Laurent Peuch <cortex@worlddomination.be>
2013-03-31 18:20:11 +0200

    [doc] add agplv3+ header

--- django_parltrack_votes_data/admin.py
+++ django_parltrack_votes_data/admin.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+
+# This file is part of django-parltrack-votes-data.
+#
+# django-parltrack-votes-data is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or any later version.
+#
+# django-parltrack-votes-data is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Affero Public
+# License along with Foobar.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2013  Laurent Peuch <cortex@worlddomination.be>
+
 from django.contrib import admin
 
 from memopol.votes import models
--- django_parltrack_votes_data/api.py
+++ django_parltrack_votes_data/api.py
@@ -1,3 +1,21 @@
+# This file is part of django-parltrack-votes-data.
+#
+# django-parltrack-votes-data is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or any later version.
+#
+# django-parltrack-votes-data is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Affero Public
+# License along with Foobar.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2013  Laurent Peuch <cortex@worlddomination.be>
+
 from tastypie.resources import ModelResource
 from memopol.votes.models import VotesData
 
